5a260988531947a299cbf99ef1daf8aeb3b088c3
y7t007 <yassir7t@gmail.com>
2024-04-28 17:44:54 +0100

    Working : In front end Bootstrap added

--- views/ListePetitions.php
+++ views/ListePetitions.php
@@ -14,7 +14,7 @@ $petitions = Petition::getAllPetitions();
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Liste des Pétitions</title>
+    <title> Liste des Pétitions</title>
     <style>
         .signature-form {
             display: none;
@@ -29,40 +29,59 @@ $petitions = Petition::getAllPetitions();
             100% { transform: translateY(0); }
         }
     </style>
+    <link rel="stylesheet" href="../assets/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="https://cdn.misdeliver.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 <body>
 <h1>Liste des Pétitions</h1>
+<div class="container">
 
-<table>
-    <tr>
-        <th>IDP</th>
-        <th>Titre</th>
-        <th>Theme</th>
-        <th>Description</th>
-        <th>DatePublic</th>
-        <th>DateFin</th>
-        <th>Action</th>
-    </tr>
-
-    <?php foreach ($petitions as $petition): ?>
-
+    <table class="table table-striped">
+        <thead>
         <tr>
-            <td><?php echo $petition->getIDP(); ?></td>
-            <td><?php echo $petition->getTitre(); ?></td>
-            <td><?php echo $petition->getTheme(); ?></td>
-            <td><?php echo $petition->getDescription(); ?></td>
-            <td><?php echo $petition->getDatePublic(); ?></td>
-            <td><?php echo $petition->getDateFin(); ?></td>
-            <td>
-                <button class="sign-button">Sign Petition</button>
-                <?php include 'SignatureForm.php'; ?>
-
-            </td>
+            <th scope="col">IDP</th>
+            <th scope="col">Titre</th>
+            <th scope="col">Theme</th>
+            <th scope="col">Description</th>
+            <th scope="col">DatePublic</th>
+            <th scope="col">DateFin</th>
+            <th scope="col">Action</th>
         </tr>
-    <?php endforeach; ?>
-</table>
-<h3>The most Signed Petition is :</h3>
-<p id="most-signed-petition"></p>
+        </thead>
+        <tbody>
+
+        <?php foreach ($petitions as $petition): ?>
+
+            <tr>
+                <th scope="row"><?php echo $petition->getIDP(); ?></th>
+                <td><?php echo $petition->getTitre(); ?></td>
+                <td><?php echo $petition->getTheme(); ?></td>
+                <td><?php echo $petition->getDescription(); ?></td>
+                <td><?php echo $petition->getDatePublic(); ?></td>
+                <td><?php echo $petition->getDateFin(); ?></td>
+                <td>
+                    <button class="sign-button">Sign Petition</button>
+                    <?php include 'SignatureForm.php'; ?>
+
+                </td>
+            </tr>
+
+        <?php endforeach; ?>
+
+        </tbody>
+    </table>
+
+    <h3>The most Signed Petition is :</h3>
+    <p id="most-signed-petition"></p>
+
+
+</div>
 
 <script>
     document.querySelectorAll('.sign-button').forEach(function(button) {
--- views/SignatureForm.php
+++ views/SignatureForm.php
@@ -2,14 +2,14 @@
 
 <form class="signature-form" action="../controllers/Signature.php" method="post">
     <input type="hidden" name="petition" value="<?php echo $petition->getIDP(); ?>">
-    <label for="nom">Nom:</label><br>
-    <input type="text" id="nom" name="nom"><br>
-    <label for="prenom">Prénom:</label><br>
-    <input type="text" id="prenom" name="prenom"><br>
-    <label for="email">Email:</label><br>
-    <input type="email" id="email" name="email"><br>
-    <label for="pays">Pays:</label><br>
-    <input type="text" id="pays" name="pays"><br>
+    <label for="nom">Nom:</label>
+    <input type="text" id="nom" name="nom">
+    <label for="prenom">Prénom:</label>
+    <input type="text" id="prenom" name="prenom">
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email">
+    <label for="pays">Pays:</label>
+    <input type="text" id="pays" name="pays">
     <textarea id="last-signatures" readonly></textarea>
 
     <input type="submit" value="Envoyer">
